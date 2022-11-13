ecd580a8ab1720a06302ab44da7ae5c24f7720f5
Orlundo Hubbard <orlundokhubbard@gmail.com>
2022-11-12 19:31:58 -0600

    adding js and api

---
+++ src/js/checker.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+const options = {
+  method: "GET",
+  url: "https://domain-checker7.p.rapidapi.com/whois",
+  params: { domain: "rapidapi.com" },
+  headers: {
+    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+    "X-RapidAPI-Host": "domain-checker7.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options)
+  .then(function(response) {
+    console.log(response.data);
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
