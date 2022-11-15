341100a3720ce50fed4fc2e7ca52015821f074d5
Orlundo Hubbard <orlundokhubbard@gmail.com>
2022-11-15 04:36:37 -0600

    more complex js

--- src/js/checker.js
+++ src/js/checker.js
@@ -1,26 +1,29 @@
-const axios = require("axios");
-const form = document.getElementById('form');
-const response = document.getElementById('checked');
+// const axios = require("axios");
+const form = document.getElementById("form");
+const response = document.getElementById("checked");
 
 function logSubmit(event) {
-
+  response.textContent = `You entered: ${event.}`;
+  event.preventDefault();
 }
 
-const options = {
-  method: "GET",
-  url: "https://domain-checker7.p.rapidapi.com/whois",
-  params: { domain: "rapidapi.com" },
-  headers: {
-    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-    "X-RapidAPI-Host": "domain-checker7.p.rapidapi.com",
-  },
-};
+form.addEventListener("submit", logSubmit);
 
-axios
-  .request(options)
-  .then(function(response) {
-    console.log(response.data);
-  })
-  .catch(function(error) {
-    console.error(error);
-  });
+// const options = {
+//   method: "GET",
+//   url: "https://domain-checker7.p.rapidapi.com/whois",
+//   params: { domain: "rapidapi.com" },
+//   headers: {
+//     "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+//     "X-RapidAPI-Host": "domain-checker7.p.rapidapi.com",
+//   },
+// };
+
+// axios
+//   .request(options)
+//   .then(function(response) {
+//     console.log(response.data);
+//   })
+//   .catch(function(error) {
+//     console.error(error);
+//   });
