2544657c830c0dd7479c1061e40010487f57413b
manuel <rrmanuel1123@gmail.com>
2022-11-20 02:05:55 -0600

    v1 search

--- src/js/checker.js
+++ src/js/checker.js
@@ -1,34 +1,52 @@
 // const axios = require("axios");
 const form = document.getElementById("form");
-const response = document.getElementById("checked");
+const results = document.getElementById("checked");
 
-function logSubmit(event) {
-  response.textContent = `You entered: ${event.}`;
+async function logSubmit(event) {
   event.preventDefault();
+
+  // Clear prev search results
+  results.innerHTML = "";
+
+  // Fetch logic
+  const searchValue = event.target.elements.search.value;
+  const url =
+    "https://knock-knock.azurewebsites.net/domain-info/" + searchValue;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  // Display results logic
+  if (data.length >= 1) {
+    // run array logic
+    data.forEach((domain) => {
+      const pElement = document.createElement("p");
+      pElement.innerText = domain.hostName;
+      results.appendChild(pElement);
+
+      const p2Element = document.createElement("p");
+      p2Element.style.borderBottom = "1px solid black";
+      if (domain.inUse) {
+        p2Element.innerText = "Not available";
+      } else {
+        p2Element.innerText = "Available";
+      }
+      results.appendChild(p2Element);
+    });
+  } else {
+    // Just one response logic
+    const pElement = document.createElement("p");
+    pElement.innerText = data.hostName;
+    results.appendChild(pElement);
+
+    const p2Element = document.createElement("p");
+    p2Element.style.borderBottom = "1px solid black";
+    if (data.inUse) {
+      p2Element.innerText = "Not available";
+    } else {
+      p2Element.innerText = "Available";
+    }
+    results.appendChild(p2Element);
+  }
 }
 
 form.addEventListener("submit", logSubmit);
-
-document.getElementById("form").addEventListener("submit", (event) => {
-  event.preventDefault();
-  response.innerHTML = "You submitted" + text.value;
-});
-
-// const options = {
-//   method: "GET",
-//   url: "https://domain-checker7.p.rapidapi.com/whois",
-//   params: { domain: "rapidapi.com" },
-//   headers: {
-//     "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-//     "X-RapidAPI-Host": "domain-checker7.p.rapidapi.com",
-//   },
-// };
-
-// axios
-//   .request(options)
-//   .then(function(response) {
-//     console.log(response.data);
-//   })
-//   .catch(function(error) {
-//     console.error(error);
-//   });
